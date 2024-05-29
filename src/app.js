64ee13ce8fc4139989d27d530306d7bb9c91271d
LuizFelipeMoreira <luizfelipemlds65@gmail.com>
2024-05-29 12:30:23 -0300

    adicionando a pasta src

--- src/app.js
+++ src/app.js
@@ -3,9 +3,12 @@ const app = express();
 const PORT = 8181;
 const useRoutes = require('./routes/userRoutes');
 const Service = require('./models/Services');
+const path = require('path');
 
+app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
-app.use(express.static('public'));
+
+app.use(express.static('./src/public'));
 
 app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
