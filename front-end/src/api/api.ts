e4023e83d7df1d38f9c90304dcb4960968239533
LuizFelipeMoreira <luizfelipemlds65@gmail.com>
2024-09-09 20:10:06 -0300

    arrumando nome de funcao

--- front-end/src/api/api.ts
+++ front-end/src/api/api.ts
@@ -5,6 +5,16 @@ export const axiosInstance = axios.create({
   baseURL: 'http://localhost:8181',
 });
 
+const handleRequest = async <T>(request: Promise<{ data: T }>): Promise<T> => {
+  try {
+    const { data } = await request;
+
+    return data;
+  } catch (error) {
+    Promise.reject(error);
+  }
+};
+
 export const GET_SERVICES = async (): Promise<ServicesType[]> => {
   try {
     const { data } = await axiosInstance.get('/services');
@@ -15,12 +25,15 @@ export const GET_SERVICES = async (): Promise<ServicesType[]> => {
   }
 };
 
-export const POST_SERVICE = async (data: ServicesType) => {
+export const CREATE_SERVICE = async (
+  data: ServicesType
+): Promise<ServicesType> => {
   try {
     const response = await axiosInstance.post('/create', data);
 
     return response.data as ServicesType;
   } catch (error) {
-    console.log(error);
+    console.log('ERRO AO CRIAR SERVICO:' + error);
+    return Promise.reject(error);
   }
 };
