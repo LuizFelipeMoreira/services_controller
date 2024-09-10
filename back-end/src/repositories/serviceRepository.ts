6426a5d822bf87950308a94c7050b180a26e9858
LuizFelipeMoreira <luizfelipemlds65@gmail.com>
2024-09-10 16:02:10 -0300

    adicionando tipagem e repositories

---
+++ back-end/src/repositories/serviceRepository.ts
@@ -0,0 +1,23 @@
+import { Servicos } from '../Models/Services';
+import { ServiceType } from '../types/ServicesType';
+
+
+export const create = async (data: ServiceType): Promise<ServiceType> => {
+     const newService = await Servicos.create(data);
+     return newService.get(); // Retorna os atributos do serviço criado
+   };
+   
+   // Função para atualizar um serviço
+   export const update = async (id: number, data: Partial<ServiceType>): Promise<[number]> => {
+     return await Servicos.update(data, { where: { id } });
+   };
+   
+   // Função para deletar um serviço
+   export const delete = async (id: number): Promise<number> => {
+     return await Servicos.destroy({ where: { id } });
+   };
+   
+   // Função para pegar todos os serviços
+   export const getAll = async (): Promise<ServiceType[]> => {
+     return await Servicos.findAll();
+   };
---
+++ back-end/src/types/ServicesType.ts
@@ -0,0 +1,8 @@
+export interface ServiceType {
+  nome: string;
+  lente: string;
+  laboratorio: string;
+  os: string;
+  // created_at: string;
+  // updated_at: string;
+}
