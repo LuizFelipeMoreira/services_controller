a8d14bf86b2de7250c6f5ee81f70284cccbf39c3
LuizFelipeMoreira <luizfelipemlds65@gmail.com>
2024-09-13 07:37:36 -0300

    corrigindo tipagem ServiceType

--- back-end/src/types/ServicesType.ts
+++ back-end/src/types/ServicesType.ts
@@ -3,6 +3,6 @@ export interface ServiceType {
   lente: string;
   laboratorio: string;
   os: string;
-  // created_at: string;
-  // updated_at: string;
+  created_at?: string;
+  updated_at?: string;
 }
