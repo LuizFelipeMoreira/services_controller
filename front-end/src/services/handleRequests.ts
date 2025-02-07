10cec735204d676d89be74fdce04c24139112926
LuizFelipeMoreira <luizfelipemlds65@gmail.com>
2025-02-07 18:01:08 -0300

    deixando arquivo de requests mais limpo

--- front-end/src/services/handleRequests.ts
+++ front-end/src/services/handleRequests.ts
@@ -1,4 +1,3 @@
-/* eslint-disable @typescript-eslint/no-unused-vars */
 import {
   IService,
   IServiceResquest,
@@ -6,72 +5,35 @@ import {
 } from '../@types/IService';
 import { axiosInstance } from '../lib/api';
 
-export const handleRequest = async <T>(
-  request: Promise<{ data: T }>
-): Promise<T> => {
+const handleRequest = async <T>(request: Promise<{ data: T }>): Promise<T> => {
   try {
     const { data } = await request;
-
     return data;
   } catch (error) {
+    console.error('API Request Error:', error);
     return Promise.reject(error);
   }
 };
 
-export const GET_SERVICES = async (
+export const GET_SERVICES = (
   offset: number,
   limit: number
-): Promise<IServicesPaginated> => {
-  try {
-    const { data } = await axiosInstance.get(
-      `/services?limit=${limit}&offset=${offset}`
-    );
+): Promise<IServicesPaginated> =>
+  handleRequest(axiosInstance.get(`/services`, { params: { limit, offset } }));
 
-    return data;
-  } catch (error) {
-    return Promise.reject(error);
-  }
-};
-
-export const GET_SERVICES_BY_NAME = async (
+export const GET_SERVICES_BY_NAME = (
   name: string,
   offset: number
-): Promise<IServicesPaginated> => {
-  try {
-    const { data } = await axiosInstance.get(
-      `http://localhost:8181/service/search?name=${name}&offset=${offset}`
-    );
+): Promise<IServicesPaginated> =>
+  handleRequest(
+    axiosInstance.get(`/service/search`, { params: { name, offset } })
+  );
 
-    return data;
-  } catch (error) {
-    return Promise.reject(error);
-  }
-};
+export const CREATE_SERVICE = (formData: IServiceResquest): Promise<IService> =>
+  handleRequest(axiosInstance.post(`/create`, formData));
 
-export const CREATE_SERVICE = async (
-  formData: IServiceResquest
-): Promise<IService> => {
-  try {
-    const { data } = await axiosInstance.post('/create', formData);
+export const DELETE_SERVICE = (id: number): Promise<void> =>
+  handleRequest(axiosInstance.post(`/delete/${id}`));
 
-    return data;
-  } catch (error) {
-    console.log('ERRO AO CRIAR SERVICO:' + error);
-    return Promise.reject(error);
-  }
-};
-
-export const DELETE_SERVICE = async (id: number): Promise<void> => {
-  const serviceDeleted = await axiosInstance.post(`delete/${id}`);
-
-  return;
-};
-
-export const UPDATE_SERVICE = async (
-  id: number,
-  body: IService
-): Promise<void> => {
-  const serviceUpdated = await axiosInstance.post(`edit/${id}`, body);
-
-  return;
-};
+export const UPDATE_SERVICE = (id: number, body: IService): Promise<void> =>
+  handleRequest(axiosInstance.post(`/edit/${id}`, body));
