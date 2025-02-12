216012c27434a80d42277651eb04984cad4c2157
LuizFelipeMoreira <luizfelipemlds65@gmail.com>
2025-02-12 12:05:12 -0300

    corrigindo bug do offset

--- back-end/src/use-cases/services/GetServiceByNameUseCase.ts
+++ back-end/src/use-cases/services/GetServiceByNameUseCase.ts
@@ -6,7 +6,8 @@ import {
 class GetServiceByNameUseCase {
     constructor(private serviceRepository: IServiceRepository) {}
 
-    async execute(name: string, offset: number): Promise<PagenatedResponse> {
+    async execute(name: string, page: number): Promise<PagenatedResponse> {
+        const offset = page > 0 ? (page - 1) * 10 : page;
         const service = await this.serviceRepository.getServiceByName(name, offset);
 
         if (!service) throw new Error('Nao ha servicos com esse nome');
