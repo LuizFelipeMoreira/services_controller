8efed321c864db9d02007fae86d311eb57b0cf3f
LuizFelipeMoreira <luizfelipemlds65@gmail.com>
2025-02-13 11:56:31 -0300

    inicializando variaveis de ambiente

--- back-end/src/database/config/database.ts
+++ back-end/src/database/config/database.ts
@@ -1,9 +1,17 @@
+/* eslint-disable @typescript-eslint/no-explicit-any */
 import { Sequelize } from 'sequelize';
-import User from '../model/Users';
+import { config } from 'dotenv';
 
-const connection = new Sequelize('servicos', 'root', 'inclo1254', {
-    host: 'localhost',
-    dialect: 'mysql',
-});
+config();
+
+const connection = new Sequelize(
+    process.env.DB_NAME as string,
+    process.env.DB_USER as string,
+    process.env.DB_PASSWORD as string,
+    {
+        host: process.env.DB_HOST as string,
+        dialect: process.env.DB_DIALECT as any,
+    }
+);
 
 export default connection;
