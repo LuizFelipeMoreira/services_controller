b987b067a01e79c4384cb86586568b3ee00f49ed
LuizFelipeMoreira <luizfelipemlds65@gmail.com>
2025-02-20 16:34:29 -0300

    alterando estrutura da tabela servicos

--- back-end/src/database/model/Services.ts
+++ back-end/src/database/model/Services.ts
@@ -6,4 +6,5 @@ export const Servicos = connection.define('servicos', {
     lente: { type: DataTypes.STRING, allowNull: false },
     laboratorio: { type: DataTypes.STRING, allowNull: false },
     os: { type: DataTypes.STRING, allowNull: false },
+    situacao: { type: DataTypes.STRING, allowNull: false, defaultValue: 'pendente' },
 });
