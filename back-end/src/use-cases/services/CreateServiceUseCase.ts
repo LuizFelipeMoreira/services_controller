b6a0aadd76ec64b6d57320e6e24a094c2ad91467
LuizFelipeMoreira <luizfelipemlds65@gmail.com>
2026-02-22 10:32:55 -0300

    style: diminuindo tabWidth 2

--- back-end/src/use-cases/services/CreateServiceUseCase.ts
+++ back-end/src/use-cases/services/CreateServiceUseCase.ts
@@ -2,13 +2,13 @@ import { CreateServiceDTO, ServiceResponseDTO } from '../../@types/IService';
 import { IServiceRepository } from '../../repositories/services-repositories/IServicesRepository';
 
 class CreateServiceUserCase {
-    constructor(private serviceRepository: IServiceRepository) {}
+  constructor(private serviceRepository: IServiceRepository) {}
 
-    async execute(data: CreateServiceDTO): Promise<ServiceResponseDTO> {
-        const newService = await this.serviceRepository.create(data);
+  async execute(data: CreateServiceDTO): Promise<ServiceResponseDTO> {
+    const newService = await this.serviceRepository.create(data);
 
-        return newService;
-    }
+    return newService;
+  }
 }
 
 export { CreateServiceUserCase };
--- back-end/src/use-cases/services/DeleteServiceUseCase.ts
+++ back-end/src/use-cases/services/DeleteServiceUseCase.ts
@@ -2,19 +2,19 @@ import { IServiceRepository } from '../../repositories/services-repositories/ISe
 import { GetServiceByIdUseCase } from './GetServiceByIdUseCase';
 
 class DeleteServiceUserCase {
-    constructor(private serviceRepository: IServiceRepository) {}
+  constructor(private serviceRepository: IServiceRepository) {}
 
-    async execute(id: number): Promise<void> {
-        const GetServiceById = new GetServiceByIdUseCase(this.serviceRepository);
-        const existingService = await GetServiceById.execute(id);
+  async execute(id: number): Promise<void> {
+    const GetServiceById = new GetServiceByIdUseCase(this.serviceRepository);
+    const existingService = await GetServiceById.execute(id);
 
-        if (!existingService) throw new Error('Service ont Found');
+    if (!existingService) throw new Error('Service ont Found');
 
-        // eslint-disable-next-line @typescript-eslint/no-unused-vars
-        const deletedService = await this.serviceRepository.deleteService(id);
+    // eslint-disable-next-line @typescript-eslint/no-unused-vars
+    const deletedService = await this.serviceRepository.deleteService(id);
 
-        return;
-    }
+    return;
+  }
 }
 
 export { DeleteServiceUserCase };
