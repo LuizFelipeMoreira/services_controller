f2cbdbd3f56b051a60fd3f418b3c4532baf8b852
LuizFelipeMoreira <luizfelipemlds65@gmail.com>
2026-02-24 15:21:12 -0300

    fix: corrigindo bug de alterar data

--- back-end/src/repositories/auth-repositories/UserRepository.ts
+++ back-end/src/repositories/auth-repositories/UserRepository.ts
@@ -2,31 +2,31 @@ import User from '../../database/model/Users';
 import { IUserRepository } from './IUserRepository';
 
 class UserRepository implements IUserRepository {
-    async createUser(name: string, email: string, password: string) {
-        const user = await User.create({
-            name,
-            email,
-            password,
-        });
+  async createUser(name: string, email: string, password: string) {
+    const user = await User.create({
+      name,
+      email,
+      password,
+    });
 
-        return user.get();
-    }
+    return user.get();
+  }
 
-    async getUserByEmail(email: string) {
-        return await User.findOne({
-            where: {
-                email,
-            },
-            raw: true,
-        });
-    }
+  async getUserByEmail(email: string) {
+    return await User.findOne({
+      where: {
+        email,
+      },
+      raw: true,
+    });
+  }
 
-    async getUserById(id: number) {
-        return await User.findOne({
-            where: { id },
-            raw: true,
-        });
-    }
+  async getUserById(id: number) {
+    return await User.findOne({
+      where: { id },
+      raw: true,
+    });
+  }
 }
 
 export default new UserRepository();
